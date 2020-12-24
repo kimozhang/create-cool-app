5d8dc58d349f40c54bba6c88a9f37490bbf60a08
kimozhang <1193055507@qq.com>
2020-12-24 19:04:10 +0800

    build: update build script

--- template-library-ts/scripts/build.js
+++ template-library-ts/scripts/build.js
@@ -3,6 +3,7 @@ const execa = require('execa')
 const chalk = require('chalk')
 
 const args = require('minimist')(process.argv.slice(2))
+const isRelease = args.release
 const env = args.e || args.env || 'production'
 const run = (bin, args, opts) => execa(bin, args, { stdio: 'inherit', ...opts })
 const step = msg => console.log(chalk.bold.yellow(msg))
@@ -10,11 +11,16 @@ const step = msg => console.log(chalk.bold.yellow(msg))
 main().catch(console.error)
 
 async function main() {
+  if (isRelease) {
+    // remove build cache for release builds to avoid outdated enum values
+    await fs.remove(path.resolve(__dirname, '../node_modules/.rts2_cache'))
+  }
+  
   const target = path.basename(process.cwd())
-  const dist = 'dist'
+  const removedDir = 'dist'
 
-  step(`\nremoving ${dist} directory...`)
-  await run('rimraf', [dist])
+  step(`\nremoving ${removedDir} directory...`)
+  await run('rimraf', [removedDir])
 
   step(`\nRolling up for ${target}...`)
   await run(
