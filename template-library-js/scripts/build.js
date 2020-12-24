e3a470d8405115bd5c6460dbf19ed870c43c60d0
kimozhang <1193055507@qq.com>
2020-12-24 11:27:29 +0800

    build: update build script

--- template-library-js/scripts/build.js
+++ template-library-js/scripts/build.js
@@ -6,9 +6,9 @@ const args = require('minimist')(process.argv.slice(2))
 const run = (bin, args, opts) => execa(bin, args, { stdio: 'inherit', ...opts })
 const step = (msg) => console.log(chalk.bold.yellow(msg))
 
-run().catch(console.error)
+main().catch(console.error)
 
-async function run() {
+async function main() {
   const env = args.e || args.env || 'production'
   const target = path.basename(process.cwd())
   const dist = 'dist'
