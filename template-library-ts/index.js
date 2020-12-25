e11c905c5e4e303ea3f6002c360d6b1fb4fa2843
kimozhang <1193055507@qq.com>
2020-12-25 15:01:43 +0800

    update

--- template-library-ts/index.js
+++ template-library-ts/index.js
@@ -1,10 +1,7 @@
 'use strict'
 
-const path = require('path')
-const name = path.basename(__dirname)
-
 if (process.env.NODE_ENV === 'production') {
-  module.exports = require(`./dist/${name}.cjs.prod.js`)
+  module.exports = require('./dist/my-library.cjs.prod.js')
 } else {
-  module.exports = require(`./dist/${name}.cjs.js`)
+  module.exports = require('./dist/my-library.cjs.js')
 }
